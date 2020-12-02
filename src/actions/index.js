54605356262578b43107fd9a1d1cbca8dbd1ca12
Roy Ge <rgecoder@gmail.com>
2020-12-02 15:55:46 -0800

    V1.24 implemented RESTful conventions with backend API server, allows create/update/delete of the streams

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,4 +1,14 @@
-import { SIGN_IN, SIGN_OUT } from "./types";
+import streams from "../apis/streams";
+import history from "../history";
+import {
+  SIGN_IN,
+  SIGN_OUT,
+  CREATE_STREAM,
+  FETCH_STREAMS,
+  FETCH_STREAM,
+  DELETE_STREAM,
+  EDIT_STREAM,
+} from "./types";
 
 export const signIn = (userId) => {
   return {
@@ -11,4 +21,39 @@ export const signOut = () => {
   return {
     type: SIGN_OUT,
   };
+};
+
+export const createStream = (formValues) => async (dispatch, getState) => {
+  const { userId } = getState().auth;
+
+  const response = await streams.post("/streams", { ...formValues, userId });
+
+  dispatch({ type: CREATE_STREAM, payload: response.data });
+  //Do some programmatic navigation to get the user back to the root route
+  history.push("/");
+};
+
+export const fetchStreams = () => async (dispatch) => {
+  const response = await streams.get("/streams");
+
+  dispatch({ type: FETCH_STREAMS, payload: response.data });
+};
+
+export const fetchStream = (id) => async (dispatch) => {
+  const response = await streams.get(`/streams/${id}`);
+
+  dispatch({ type: FETCH_STREAM, payload: response.data });
+};
+
+export const editStream = (id, formValues) => async (dispatch) => {
+  const response = await streams.patch(`/streams/${id}`, formValues); //edit needs both id and formValues
+
+  dispatch({ type: EDIT_STREAM, payload: response.data });
+  history.push("/");
+};
+
+export const deleteStream = (id) => async (dispatch) => {
+  await streams.delete(`/streams/${id}`); //don't need response for delete
+
+  dispatch({ type: DELETE_STREAM, payload: id });
 };
--- src/components/streams/StreamCreate.js
+++ src/components/streams/StreamCreate.js
@@ -1,73 +1,25 @@
 import React from "react";
-import { Field, reduxForm } from "redux-form";
+
+import { connect } from "react-redux";
+import { createStream } from "../../actions";
+
+import StreamForm from "./StreamForm";
 
 class StreamCreate extends React.Component {
-  renderError = ({ error, touched }) => {
-    if (touched && error) {
-      return (
-        <div className="ui error message">
-          <div className="header">{error}</div>
-        </div>
-      );
-    }
-  };
-
-  renderInput = ({ input, label, meta }) => {
-    const className = `field ${meta.error && meta.touched ? "error" : ""}`;
-
-    return (
-      <div className={className}>
-        <label>{label}</label>
-        <input
-          {...input}
-          autoComplete="off"
-          // {...formProps.input}
-          // onChange={formProps.input.onChange}
-          // value={formProps.input.value}
-        />
-        {/* <div>{meta.error}</div> */}
-        {this.renderError(meta)}
-      </div>
-    );
-  };
-
-  onSubmit(formValues) {
+  onSubmit = (formValues) => {
     // event.preventDefault(); //usually do it like this, not with redux-form
-    console.log(formValues); //w/e values that was entered into the form
-  }
+    // console.log(formValues); //w/e values that was entered into the form
+    this.props.createStream(formValues);
+  };
 
   render() {
     return (
-      <form
-        onSubmit={this.props.handleSubmit(this.onSubmit)}
-        className="ui form error"
-      >
-        <Field name="title" component={this.renderInput} label="Enter Title" />
-        <Field
-          name="description"
-          component={this.renderInput}
-          label="Enter Description"
-        />
-        <button className="ui button primary">Submit</button>
-      </form>
+      <div>
+        <h3>Create a Stream</h3>
+        <StreamForm onSubmit={this.onSubmit} />
+      </div>
     );
   }
 }
 
-const validate = (formValues) => {
-  const errors = {};
-  if (!formValues.title) {
-    //only ran if the user did not enter a title
-    errors.title = "You must enter a title";
-  }
-
-  if (!formValues.description) {
-    errors.description = "You must enter a description";
-  }
-  return errors;
-};
-
-export default reduxForm({
-  form: "streamCreate",
-  validate,
-})(StreamCreate);
+export default connect(null, { createStream })(StreamCreate);
